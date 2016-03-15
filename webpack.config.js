86486678994fdba3ad27450bd55d56d563ad60ac
German Nemes <gnemes@Germans-MacBook-Pro.local>
2016-03-15 14:10:19 -0300

    ReactJS refactor

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,46 @@
-module.exports = {
-    entry: [
-        './web/example1/app.js'
-    ],
+var webpack = require('webpack');
+
+/*
+ * Default webpack configuration for development
+ */
+var config = {
+    devtool: 'eval-source-map',
+    entry: __dirname + "/web/example1/App.js",
     output: {
-        path: __dirname,
-        filename: "./web/example1/bundle.js"
+        path: __dirname + "/web",
+        filename: "bundle.js"
     },
     module: {
         loaders: [{
-            test: /\.jsx?$/,
-            loader: 'babel'
-        }]
-    }
-};
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }]
+    },
+    devServer: {
+        contentBase: "./web",
+        colors: true,
+        historyApiFallback: true,
+        inline: true
+    },
+}
+
+/*
+ * If bundling for production, optimize output
+ */
+if (process.env.NODE_ENV === 'production') {
+    config.devtool = false;
+    config.plugins = [
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.UglifyJsPlugin({comments: false}),
+        new webpack.DefinePlugin({
+            'process.env': {NODE_ENV: JSON.stringify('production')}
+        })
+    ];
+}
+;
+
+module.exports = config;
